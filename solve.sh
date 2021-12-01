9e755777200404c88247ddf57ef9ff3de93a5ae6
Uygar Uyaniksoy <uygaruyaniksoy@gmail.com>
2021-12-01 13:36:19 +0100

    fetch input automatically

--- solve.sh
+++ solve.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# if the folder is not created for the day create from the template
 [ -d $1 ] || cp -r 00 $1
+
+# if cookie specified fetch the input from aoc directly
+# specify cookie by => $ export AOC_COOKIE="session=....."
+[ $AOC_COOKIE ] && curl --cookie "$AOC_COOKIE" https://adventofcode.com/2021/day/1/input > "$1/input.txt"
+
+# change into solution directory and execute solution.ts
+# while listening to changes in the folder
 cd $1 && ls | entr ./solution.ts
 
